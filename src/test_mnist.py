3cfad019d4dc8dacd5a29aae2054592ba09f8ed6
Isaias Gonzalez <igonzalez@gobdigital.com>
2022-07-14 18:02:37 -0500

    [UPDATE][test_mnist] Se implemento el acurracy

--- src/test_mnist.py
+++ src/test_mnist.py
@@ -31,24 +31,12 @@ print(f"Y_VAL={Y_VAL.shape}")
 # Create NN
 NN = Utils.loadModel('./models/mnist.npz')
 
-loss = []
-# Evaluate
-for (x, y) in createMinibatches(X_VAL, Y_VAL, 10):
-    pY = NN.run(x)
-
-    cost = CostFunction.xe[0](pY, y)
-    loss.append(cost)
-
-    print("----------------------------------------------------------------------")
-    print(f"Propuesto: {pY.argmax(axis=0)}, Real: {y[0]}, Costo: {cost}")
-
-totalCost = np.sum(loss)/len(loss)
-print("")
-print("")
+totalCost = Utils.accuracy(NN, X_VAL, Y_VAL)
+print()
 print("----------------------------------------------------------------------")
 print("----------------------------------------------------------------------")
-print(f"Medial Cost: {totalCost}")
-print("")
-print("")
+print()
+print(f"Acurracy: {totalCost}")
+print()
 print("----------------------------------------------------------------------")
 print("----------------------------------------------------------------------")
